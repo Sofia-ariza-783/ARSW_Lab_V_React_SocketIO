459176ff7aa503031c63741882c9216c4ddd2145
Sofia <Sofia>
2026-03-09 16:41:05 -0500

    feat(canvas): Implement P5.js drawing functionality

---
+++ socketIO-Canvas/App.jsx
@@ -0,0 +1,63 @@
+import './App.css';
+import {useEffect, useRef, useState} from 'react';
+import p5 from "p5";
+import {Brush} from "lucide";
+
+function App() {
+    const [lineWidth, setLineWidth] = useState(5);
+    const myp5 = useRef(null);
+
+    const sketch = function (p) {
+        let lastX, lastY;
+        let picker;
+
+        p.setup = function () {
+            let color = p.color(p.random(255),p.random(255), p.random(255));
+            p.createCanvas(window.innerWidth, window.innerHeight);
+            p.strokeWeight(lineWidth);
+            picker = p.createColorPicker(color);
+            picker.position(10, 10);
+        }
+
+        p.draw = function () {
+            if (p.mouseIsPressed === true) {
+
+                if (lastX === undefined) {
+                    lastY = p.mouseY;
+                    lastX = p.mouseX;
+                }
+
+                p.stroke(picker.color());
+                p.line(lastX, lastY, p.mouseX, p.mouseY)
+                lastY = p.mouseY;
+                lastX = p.mouseX;
+            } else {
+                lastY = undefined;
+                lastX = undefined;
+            }
+        }
+
+    };
+
+    useEffect(() => {
+        if (!myp5.current) {
+            myp5.current = new p5(sketch,"container");
+        }
+    }, []);
+
+    return (
+        <div>
+            <main className="canvasContainer">
+                <div className="header">
+                    Dibujo Online P5
+                </div>
+
+                <hr/>
+                <div id="container"></div>
+                <hr/>
+            </main>
+        </div>
+    );
+}
+
+export default App
